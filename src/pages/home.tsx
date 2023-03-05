38f8073b9498014512bf5c6afc9d845987712244
Ratchasri Kidking <orc_hero@hotmail.com>
2023-03-05 17:55:26 +0700

    fix

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,48 @@
-import React from 'react'
-import { useList } from '@pankod/refine-core'
+import React from "react"
+import { useList } from "@pankod/refine-core"
+import {
+  PieChart,
+  PropertyReferrals,
+  TotalRevenue,
+  PropertyCard,
+  TopAgent,
+} from "components"
+import { Box, Typography } from "@pankod/refine-mui"
 
 const Home = () => {
   return (
-    <div>
-      Home 
-    </div>
+    <Box>
+      <Typography fontSize={25} fontWeight={700} color="#11142D">
+        Dashboard
+      </Typography>
+
+      <Box mt="20px" display="flex" gap={4} flexWrap="wrap">
+        <PieChart
+          title="Properties for sale"
+          value={684}
+          series={[75, 25]}
+          colors={["#475be8", "#e4e8ef"]}
+        />
+        <PieChart
+          title="Properties for Rent"
+          value={550}
+          series={[75, 25]}
+          colors={["#475be8", "#e4e8ef"]}
+        />
+        <PieChart
+          title="Properties for Total Customer"
+          value={684}
+          series={[75, 25]}
+          colors={["#475be8", "#e4e8ef"]}
+        />
+        <PieChart
+          title="Properties for Cities"
+          value={684}
+          series={[75, 25]}
+          colors={["#475be8", "#e4e8ef"]}
+        />
+      </Box>
+    </Box>
   )
 }
 
